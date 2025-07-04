bb7e50873c56dfc993f4e083c9d77f4b1d7f43d9
Rohith-Kaki <rohithlahari55@gmail.com>
2025-07-04 22:18:05 +0530

    feat: Continuous Deployment

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ def predict(request:Request, file:UploadFile=File(...)):
         raise CustomException(e, sys)
     
 if __name__ == "__main__":
-    uvicorn.run(app, host= "localhost", port = 8000)
+    uvicorn.run(app, host= "0.0.0.0", port = 8000)
